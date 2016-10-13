48b57c739b9b53bb4d2bea37c9a82094a334e7d6
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-10-13 00:41:29 -0700

    fake trainers

--- app/models/tokimon.rb
+++ app/models/tokimon.rb
@@ -1,14 +1,14 @@
 class Tokimon < ApplicationRecord
 	belongs_to :trainer
 	validates :name, presence: true, length: { maximum: 20 }
-	validates :height, presence: true, length: { maximum: 20 }
-	validates :weight, presence: true, length: { maximum: 20 }
-	validates :fire, presence: true, length: { maximum: 20 }
-	validates :fight, presence: true, length: { maximum: 20 }
-	validates :fly, presence: true, length: { maximum: 20 }
-	validates :water, presence: true, length: { maximum: 20 }
-	validates :electric, presence: true, length: { maximum: 20 }
-	validates :frozon, presence: true, length: { maximum: 20 }
+	validates :height, presence: true, length: { maximum: 4 }
+	validates :weight, presence: true, length: { maximum: 4 }
+	validates :fire, presence: true, length: { maximum: 4 }
+	validates :fight, presence: true, length: { maximum: 4 }
+	validates :fly, presence: true, length: { maximum: 4 }
+	validates :water, presence: true, length: { maximum: 4 }
+	validates :electric, presence: true, length: { maximum: 4 }
+	validates :frozon, presence: true, length: { maximum: 4 }
 	validates :trainer_id, presence: true
 	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Trainer.create!(name:  "Frank", age: 20, height: 180, weight: 170, level: 0, address: "pandora dr",
-             email: "zhwtf@gmail.com")
+#Trainer.create!(name:  "Frank", age: 20, height: 180, weight: 170, level: 0, address: "pandora dr",
+             #email: "zhwtf999@gmail.com")
+
+10.times do |n|
+  name  = Faker::Name.first_name 
+  email = "example-#{n+1}@gmail.com"
+  age = Faker::Number.between(10, 50)
+  height = Faker::Number.between(170, 200)
+  weight = Faker::Number.between(100, 200)
+  address = Faker::Address.street_address
+
+
+  Trainer.create!(name:  name,
+               email: email,
+               age: age,
+               height: height,
+               weight: weight,
+               level: 0,
+               address: address)
+               
+end
+
+trainers = Trainer.all
+2.times do
+  
+  trainers.each { |trainer| 
+  	name  = Faker::Name.first_name
+  weight = Faker::Number.between(10, 100)
+  height = Faker::Number.between(1, 200)
+  fly = Faker::Number.between(0, 100)
+  fight = Faker::Number.between(0, 100)
+  fire = Faker::Number.between(0, 100)
+  water = Faker::Number.between(0, 100)
+  electric = Faker::Number.between(0, 100)
+  frozon = Faker::Number.between(0, 100)
+  total = fly + fight + fire + water + electric + frozon
+  	trainer.tokimons.create!(name: name, height: height,
+               weight: weight, fly: fly, fight: fight, fire: fire, water: water, electric: electric, frozon: frozon, total: total
+  	) }
+end
+
