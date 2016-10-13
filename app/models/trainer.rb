b4ea54aa6d592e1b8f1b7ef3543a601b06f8b428
郑昊 <zhenghao@d207-023-191-066.wireless.sfu.ca>
2016-10-13 14:41:36 -0700

    improved3

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,5 @@
 class Trainer < ApplicationRecord
-	has_many :tokimons
+	has_many :tokimons, dependent: :destroy
 	validates :name, presence: true, length: { maximum: 20 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
